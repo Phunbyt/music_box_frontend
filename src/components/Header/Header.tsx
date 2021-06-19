fada370c1db5081e544e440b565004f10706d926
Aderemi Amos <aderemi.amos@decagon.dev>
2021-06-19 15:29:01 +0100

    boiler plate

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={classes.Header}>
+            <h2 className={classes.Text}>Music Box</h2>
+        </div>
+    )
+}
+
+export default Header
