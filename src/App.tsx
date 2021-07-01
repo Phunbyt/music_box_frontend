add99f45455e551163838ad94fa05139d3c6086a
Phunbyt <roluwafunbi@gmail.com>
2021-07-01 20:06:41 +0100

    test and play commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import Homepage from './components/Homepage/Homepage';
 import ArtistsResults from './components/Homepage/ArtistsResults/ArtistsResults';
 import AlbumResults from './components/Homepage/AlbumsResults/AlbumResults'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PlayerState from './Contexts/playerState';
 
 
 const App = () => {
   return (
     <>
+      <PlayerState>
+
       <BrowserRouter>
         <Switch>
           <Route exact path='/home' component={Homepage} />
@@ -15,6 +18,7 @@ const App = () => {
           <Route exact path='/albumdetails' component={AlbumResults} />
         </Switch>
       </BrowserRouter>  
+      </PlayerState>
     </>
   );
 }
--- src/components/Homepage/Flow/Flow.tsx
+++ src/components/Homepage/Flow/Flow.tsx
@@ -2,21 +2,39 @@ import "./Flow.css";
 import { FaEllipsisH, FaPlay, FaPlayCircle, FaPlusCircle } from "react-icons/fa";
 import flowcover from './flowcover.png';
 import flowcoverTwo from "./flowcover2.png";
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+
+import axios from 'axios'
+import playerContext from "../../../Contexts/playerContext";
 
 interface Props{
 
 }
 export default function Flow() {
+  const { handleClick } = useContext(playerContext)
+  const [songs, setSongs] = useState([] as Record<string, any>[])
+  useEffect(() => {
+axios
+  .get("https://music-box-a.herokuapp.com/listeninghistory")
+  .then((res) => {
+    console.log(res, "ressss")
+    setSongs(res.data);
+    console.log(res.data)
+  });
+  },[])
   return (
     <div className="flow">
       <div className="top">
         <p className="recently">Flow</p>
-        <p><i className="dots fa fa-ellipsis-h" /></p>
-        </div>
+        <p>
+          <i className="dots fa fa-ellipsis-h" />
+        </p>
+      </div>
       <div className="card">
         <div className="front-image"></div>
-        <div className="image"><i className="play fa fa-play-circle"/></div>
+        <div className="image" onClick={() => handleClick(songs, songs.length-1)}>
+          <i className="play fa fa-play-circle" />
+        </div>
       </div>
     </div>
   );
